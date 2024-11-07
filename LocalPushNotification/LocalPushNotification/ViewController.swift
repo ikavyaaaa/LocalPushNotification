7730825de70181acad5879de4ef0f49206d42fb0
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-11-07 16:56:13 +0530

    Initial commit

---
+++ LocalPushNotification/LocalPushNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalPushNotification
+//
+//  Created by Kavya Krishna K. on 07/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LocalPushNotification/LocalPushNotificationTests/LocalPushNotificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalPushNotificationTests.swift
+//  LocalPushNotificationTests
+//
+//  Created by Kavya Krishna K. on 07/11/24.
+//
+
+import Testing
+@testable import LocalPushNotification
+
+struct LocalPushNotificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
