ec0761fb6367dd45b6cf972e0f23e62e63ce808b
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-11-07 17:00:07 +0530

    kSceneDelegate

---
+++ LocalPushNotification/LocalPushNotification/Supporting/Constants/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  LocalPushNotification
+//
+//  Created by Kavya Krishna K. on 07/11/24.
+//
+
+import Foundation
+import UIKit
+
+
+let kScreenSize = UIScreen.main.bounds
+let kScreenWidth = UIScreen.main.bounds.width
+let kScreenHeight = UIScreen.main.bounds.height
+let kAppDelegate = UIApplication.shared.delegate as! AppDelegate
+var kSceneDelegate: SceneDelegate?
+var UserInterfaceStyle: UIUserInterfaceStyle = .light
