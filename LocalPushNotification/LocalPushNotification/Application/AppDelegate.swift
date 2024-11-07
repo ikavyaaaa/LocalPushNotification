961559289347093b9942a69f5ca7465d42729358
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-11-07 17:07:27 +0530

    local push notification

--- LocalPushNotification/LocalPushNotification/Application/AppDelegate.swift
+++ LocalPushNotification/LocalPushNotification/Application/AppDelegate.swift
@@ -6,56 +6,70 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Request notification permissions at launch
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.badge, .sound, .alert]) { granted, error in
+            if granted {
+                print("User granted notification permission")
+            } else if let error = error {
+                print("Failed to request notification permission: \(error.localizedDescription)")
+            }
+        }
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        // Called when the user discards a scene session
     }
     
-    func postNotification(_ title : String, body : String) {
+    func postNotification(_ title: String, body: String) {
         let center = UNUserNotificationCenter.current()
-        center.delegate = self
-        center.requestAuthorization(options: [.badge,.sound,.alert]) { granted, error in
-            if error == nil {
-                print("User permission is granted : \(granted)")
-            }
-        }
+        
         let content = UNMutableNotificationContent()
         content.title = title
         content.body = body
+        content.sound = .default
+        
         let date = Date().addingTimeInterval(1)
-        let dateComponent = Calendar.current.dateComponents([.year,.month,.day,.hour,.minute,.second], from: date)
+        let dateComponent = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
         
         let uuid = UUID().uuidString
         let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
         
         center.add(request) { error in
+            if let error = error {
+                print("Failed to add notification request: \(error.localizedDescription)")
+            }
         }
     }
 
-
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
+    // MARK: - UNUserNotificationCenterDelegate Methods
     
+    // This method is called when a notification is delivered while the app is in the foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // Display the notification alert and sound even if the app is in the foreground
+        completionHandler([.alert, .sound])
+    }
+    
+    // This method is called when a user taps on the notification
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // Handle the notification response here
+        print("User tapped on notification: \(response.notification.request.content.body)")
+        completionHandler()
+    }
 }
